fe2ef416e7722087cbb9d679ffbe70953240d9ac
Mark Thiel <mthiel117@gmail.com>
2023-07-16 23:11:29 -0400

    files

---
+++ get-sample-files.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Sample Files for Git section - CI Workshop Fundamentals
+if [ -d "/home/coder/project/labfiles/samplefiles" ]
+then
+    rm -rf /home/coder/project/labfiles/samplefiles
+fi
+cd /home/coder/project/labfiles/
+curl --remote-name-all https://github.com/mthiel117/HNB/raw/main/samplefiles.tar
+tar -xf samplefiles.tar
+rm samplefiles.tar
